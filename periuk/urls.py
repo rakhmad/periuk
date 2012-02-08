4dbd160165f77d27d6be6a7e91ae3406e6fcc539
Rakhmad Azhari <rakhmad@csui01.org>
2012-02-08 12:45:55 +0700

    Adding Admin page route

--- periuk/urls.py
+++ periuk/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 
+from projects import views
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', views.home),
     # Examples:
     # url(r'^$', 'periuk.views.home', name='home'),
     # url(r'^periuk/', include('periuk.foo.urls')),
@@ -13,5 +15,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
